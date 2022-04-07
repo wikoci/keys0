9fe15770c993bb8a5ba2163665864fee675e56f5
wikoci <74468439+wikoci@users.noreply.github.com>
2022-04-07 08:17:26 +0200

    push

--- telegram/index.js
+++ telegram/index.js
@@ -8,49 +8,49 @@ const Chris = 1304170139;
 const Ola = 1429023572;
 const Omar = 1650196085;
 const depa = 873423089;
-const otherChatId = [1420109196];
+const otherChatId = [1420109196, Omar];
 // Ola 1429023572
 const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(token, { polling: true });
 
 function initBot() {
-  console.log("Bot events listening");
-  bot.on("polling_error", (err) => console.log());
-  bot.on("message", (msg) => {
-    console.log(msg);
-    //console.log("___", msg);
-    //     otherChatId.filter((id) => {
-    //       console.log("ID FOR CHRIS", id);
-    bot.sendMessage(Chat_id, JSON.stringify(msg));
-    // });
-  });
+    console.log("Bot events listening");
+    bot.on("polling_error", (err) => console.log());
+    bot.on("message", (msg) => {
+        console.log(msg);
+        //console.log("___", msg);
+        //     otherChatId.filter((id) => {
+        //       console.log("ID FOR CHRIS", id);
+        bot.sendMessage(Chat_id, JSON.stringify(msg));
+        // });
+    });
 }
 
 async function messageViaTelegram(content) {
-  otherChatId.filter(async (id) => {
-    console.log("Send to id " + id);
-    await fetch(URL, {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        chat_id: id,
-        text: content || "__init__ ",
-        parse_mode: "HTML",
-      }),
-    })
-      .then((e) => e.json())
-      .then((e) => {
-        //console.log("It ok ", e);
-      })
-      .catch((err) => {
-        console.log("Errr ", err);
-      });
-  });
+    otherChatId.filter(async(id) => {
+        console.log("Send to id " + id);
+        await fetch(URL, {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({
+                    chat_id: id,
+                    text: content || "__init__ ",
+                    parse_mode: "HTML",
+                }),
+            })
+            .then((e) => e.json())
+            .then((e) => {
+                //console.log("It ok ", e);
+            })
+            .catch((err) => {
+                console.log("Errr ", err);
+            });
+    });
 }
 
 module.exports = {
-  messageViaTelegram,
-  initBot,
+    messageViaTelegram,
+    initBot,
 };
